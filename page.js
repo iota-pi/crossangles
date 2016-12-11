0b1ebb927288570d6af8c60737edf83db7aa4bde
David <wofs12@gmail.com>
2016-12-11 22:39:43 +1100

    Added initial JS

---
+++ page.js
@@ -0,0 +1,17 @@
+/* page.js
+ *
+ * JS file to handle animations, etc.
+ *
+ * Authors: David Adams
+ */
+
+/* --- JSLint Options --- */
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+(function () {
+    "use strict";
+    $(document).ready(function () {
+        /* code goes here */
+    });
+}());
