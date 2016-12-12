1a5aa9ea00525cf4c280413e495a194b0bc3098b
David <wofs12@gmail.com>
2016-12-12 15:32:58 +1100

    Added typeahead and jquery

--- page.js
+++ page.js
@@ -11,9 +11,39 @@
 
 (function () {
     "use strict";
-    $(document).ready(function () {
-        function addcourse() {
-            var inputfield = $('input#coursein');
-        }
+    var courses;
+    
+    function init() {
+        var matcher = function (strs) {
+            return function findMatches(q, cb) {
+                var matches = [];
+                $.each(strs, function (i, str) {
+                    if (str.toLowerCase().indexOf(q.toLowerCase()) !== -1) {
+                        matches.push(str);
+                    }
+                });
+                cb(matches);
+            };
+        };
+        $('.typeahead#coursein').typeahead({
+            hint: true,
+            highlight: true,
+            minLength: 1
+        }, {
+            name: 'courses',
+            source: matcher(courses)
+        });
+    }
+    
+    // Load course data from courses.json
+    $.getJSON('courses.json', function (json) {
+        courses = json;
+        $(document).ready(function () {
+            init();
+        });
     });
+    
+    function addcourse() {
+        var inputfield = $('input#coursein');
+    }
 }());
