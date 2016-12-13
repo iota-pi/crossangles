66dc21818e0b971e1f9adab40844ff7203b8223b
David <wofs12@gmail.com>
2016-12-13 12:31:01 +1100

    Starting point PHP code

---
+++ scraper.php
@@ -0,0 +1,13 @@
+<?php
+/* scraper.php
+ *
+ * This script is to scrape data from the UNSW timetable
+ * Course codes and names go to courses.json
+ * Timetable data goes to timetable.json
+ *
+ * Authors: David Adams
+ */
+
+
+
+?>
