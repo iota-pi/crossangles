c48fbbad86728e2e305f4d97e0323ee112d02f13
David <david.adams.16.12@gmail.com>
2016-12-16 09:26:58 +1100

    Timetable grid

--- js/timetable.js
+++ js/timetable.js
@@ -1,6 +1,6 @@
 /* timetable.js
  *
- * Handles display (and maybe generation?) of timetables
+ * Handles the display (and maybe generation?) of timetables
  */
 
 /* --- JSLint Options --- */
@@ -10,11 +10,27 @@
 $(document).ready(function () {
     "use strict";
     // Create SVG
-    var draw = SVG('timetable').size('100%', '100%');
+    var draw = SVG('timetable').size(690, 800),
+        w = $('#timetable svg').width(),
+        h = Math.floor(w / 690 * 800);
+    draw.size(w, h);
     
     // Fix subpixel offset
     draw.spof();
     SVG.on(window, 'resize', function () { draw.spof(); });
     
-    
+    // Draw grid
+    (function drawGrid() {
+        var x = 90, days  = 5, xstep = Math.floor((w - x) / days),
+            y = 44, hours = 14, ystep = Math.floor((h - y) / hours), i;
+        
+        // Vertical lines
+        for (i = 0; i < days; i += 1) {
+            draw.line(x + xstep * i, 0, x + xstep * i, h).stroke({ width: 1, color: 'black' });
+        }
+        // Horizontal lines
+        for (i = 0; i < hours; i += 1) {
+            draw.line(0, y + ystep * i, w, y + ystep * i).stroke({ width: 1, color: 'black' });
+        }
+    }());
 });
