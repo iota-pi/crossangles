6ccf9ba0ed86e9762032d99f74230167be13fd2f
David <wofs12@gmail.com>
2016-12-15 11:43:38 +1100

    Updated scraper

--- scraper.py
+++ scraper.py
@@ -5,11 +5,20 @@ import requests
 import json
 import time
 
+YEAR = '2017'
+
 def main():
+    # TEMP
+    page = loadPage('http://timetable.unsw.edu.au/2017/ACCT1501.html')
+    getTimetable(page)
+    return
+    # END TEMP
     scrape()
 
 def scrape():
+    global YEAR
     courses = {}
+    timetable = {}
     
     # Get faculty pages
     fpages = getPages()
@@ -17,33 +26,43 @@ def scrape():
         # Get course pages
         pages = getPages(fpage)
         
-        for page in pages:
+        for uri_end in pages:
+            page = loadPage('http://timetable.unsw.edu.au/' + YEAR + '/' + uri_end)
             course = getCourseName(page)
-            print(*course)
             courses[course[0]] = course[1]
+            data = getTimetable(page)
+            timetable[course[0]] = data
             
         if len(courses) != 0:
             with open('data/courses.json', 'w') as f:
                 json.dump(courses, f)
                 print("JSON dumped")
-            
+        
         # ... a polite little delay
         politeDelay()
 
 def getPages(fpage='subjectSearch.html'):
     global YEAR
-    page = getURI('http://timetable.unsw.edu.au/' + YEAR + '/' + fpage)
-    tree = html.fromstring(page.content)
+    tree = loadPage('http://timetable.unsw.edu.au/' + YEAR + '/' + fpage)
     links = tree.xpath('//tr/td[1][@class="data"]/a[contains(@href,".html")]/@href')
     return links
-    
-def getCourseName(cpage):
-    global YEAR
-    page = getURI('http://timetable.unsw.edu.au/' + YEAR + '/' + cpage)
-    tree = html.fromstring(page.content)
-    text = tree.xpath('//td[@class="classSearchMinorHeading"]/text()')
+
+def getCourseName(page):
+    text = page.xpath('//td[@class="classSearchMinorHeading"]/text()')
     return text[0].split(' ', 1)
-    
+
+def getTimetable(page):
+    activity = tree.xpath('//tr/td[1][@class="data"]/a[contains(@href,".html")]/text()')
+    period   = tree.xpath('//tr/td[2][@class="data"]/a[contains(@href,".html")]/text()')
+    space    = tree.xpath('//tr/td[6][@class="data"]/text()')
+    time     = tree.xpath('//tr/td[7][@class="data"]/text()')
+    print(activity, period, space, time)
+
+def loadPage(uri):
+    page = getURI(uri)
+    tree = html.fromstring(page.content)
+    return tree
+
 def getURI(uri):
     return requests.get(uri)
 
@@ -51,5 +70,4 @@ def politeDelay(s=1):
     time.sleep(s)
 
 if __name__ == '__main__':
-    YEAR = '2017'
     main()
