86596072a03f12e5f8506c5f031153075d81fc02
David <david.adams.16.12@gmail.com>
2017-02-24 22:57:08 +1100

    Pass linting rules

--- js/generate.js
+++ js/generate.js
@@ -14,27 +14,27 @@
 
 function generateTimetable(data) {
     // data = {code: [[class_type, status, enrolments, [[class_time, class_weeks, class_locations], ...]], ...]}
+    'use strict';
 
     function makeList(data) {
-        var list = [];
+        var list = [], coursecode, coursedata, classdata, timedata, classtime, i, j;
 
-        for (var coursecode in data) {
+        for (coursecode in data) {
             // Loop through only properties which are not inherited
             if (data.hasOwnProperty(coursecode)) {
-                var coursedata = data[coursecode];
-                for (var i = 0; i < coursedata.length; i += 1) {
-                    var classdata = coursedata[i],
-                        timedata  = classdata[3],
-                        classtime = [];
-                    for (var j = 0; j < timedata.length; j += 1) {
+                coursedata = data[coursecode];
+                for (i = 0; i < coursedata.length; i += 1) {
+                    classdata = coursedata[i];
+                    timedata  = classdata[3];
+                    classtime = [];
+                    for (j = 0; j < timedata.length; j += 1) {
                         classtime.push(timedata[j][0]);
                     }
                     classtime = classtime.join(',');
 
                     // Make an entry in the list
                     // Format = [course_code, class_time, class_type, status, enrolments]
-                    var entry = [coursecode, classtime, coursedata[0], coursedata[1], coursedata[2]];
-                    list.push(entry);
+                    list.push([coursecode, classtime, coursedata[0], coursedata[1], coursedata[2]]);
                 }
             }
         }
@@ -46,9 +46,6 @@ function generateTimetable(data) {
     function heuristic() {
 
     }
-
-    // Use strict js rules
-    'use strict';
 
     // Create the list of class time options
     var list = makeList();
