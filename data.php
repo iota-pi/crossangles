989bf9706ad0a277f6c1aa3d0ff77b5703cbdce3
David <david.adams.16.12@gmail.com>
2017-02-25 13:39:07 +1100

    Added data.php to fetch json entries

---
+++ data.php
@@ -0,0 +1,14 @@
+<?php
+
+$courses = json_decode($_GET['courses']);
+
+$data = json_decode(file_get_contents('data/timetable.json'), true);
+
+$output = [];
+foreach ($courses as $course) {
+    $output[$course] = $data[$course];
+}
+
+echo json_encode($output);
+
+?>
