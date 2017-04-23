1522243c3fb8c6a4907382cc5f6fcd9a6214d3e9
David <david.adams.16.12@gmail.com>
2017-04-24 09:53:34 +1000

    Moved timetable evaluation to evaluate.js

---
+++ js/evaluate.js
@@ -0,0 +1,151 @@
+/* evaluate.js
+ * Defines evaluation functions for scoring timetable
+ */
+
+function scoreTime(start, end) {
+    'use strict';
+
+    var score = 4 - Math.abs(14.5 - (start + end) / 2);
+    score *= Math.abs(score);
+    score *= (end - start);
+
+    return score;
+}
+
+function timetableToArray(indexTimetable, streams) {
+    'use strict';
+
+    var timetableData = indexTimetable.map(function (x, i) { return streams[i][x]; }),
+        timetable = [[], [], [], [], []],
+        day_of_week = ['M', 'T', 'W', 'H', 'F'],
+        i,
+        j,
+        times,
+        time,
+        day,
+        hour;
+
+    // Convert to timetable array
+    for (i = 0; i < timetableData.length; i += 1) {
+        times = timetableData[i][0];
+        for (j = 0; j < times.length; j += 1) {
+            time = times[j];
+            day = day_of_week.indexOf(time[0]);
+            for (hour = time[1]; hour < time[2]; hour += 0.5) {
+                // Initialise array element if required to blank array (this array will hold course codes for each component in this space)
+                if (timetable[day][hour * 2] === undefined) {
+                    timetable[day][hour * 2] = [];
+                }
+
+                // Append this course code to any previous course codes in this half-hour slot
+                timetable[day][hour * 2].push(timetableData[i][3]); // element 3 is course code
+            }
+        }
+    }
+
+    return timetable;
+}
+
+function scoreFreeDays(timetable) {
+    'use strict';
+    var freeDayScores = { M: 120, T: 100, W: 180,  H: 100, F: 150 },
+        i;
+
+    for (i = 0; i < timetable.length; i += 1) {
+        // This day is free (no classes) if the length of the array is 0
+        if (timetable[i].length === 0) {
+
+        }
+    }
+}
+
+function scoreTimetable(indexTimetable, streams) {
+    'use strict';
+    if (indexTimetable === null) { return null; }
+
+    var timetable = timetableToArray(indexTimetable, streams),
+        score = 0;
+
+    score += scoreFreeDays(timetable);
+}
+
+/*
+function evaluateTimetable(indexTimetable, streams) {
+    'use strict';
+
+    if (indexTimetable === null) { return null; }
+    var timetable = indexTimetable.map(function (x, i) { return streams[i][x]; }),
+        score = 0,
+        i,
+        j,
+        k,
+        l,
+        times,
+        time,
+        cbsDays = { M: false, T: false, W: false, H: false, F: false },
+        uniDays = { M: false, T: false, W: false, H: false, F: false },
+        cbsTimes = [],
+        day,
+        duration,
+    // Scores for free days
+        freeScores = { M: 120, T: 100, W: 180,  H: 100, F: 150 },
+    // Scores for CBS events
+        tbtClass = 100,
+        coreClass = 70,
+        bibleClass = 100,
+    // Score for CBS event being immediately before or after another class on campus
+        close2CBS = 100,
+    // Score for clashes
+        clash = -500;
+
+    for (i = 0; i < timetable.length; i += 1) {
+        times = timetable[i][0];
+
+        for (j = 0; j < times.length; j += 1) {
+            time = times[j];
+
+            // Update CBS / Uni days
+            if (timetable[i][3] === 'CBS') {
+                cbsDays[time[0]] = true;
+                cbsTimes.push(time);
+            } else {
+                uniDays[time[0]] = true;
+            }
+
+            // Score time of day
+            score += scoreTime(time[1], time[2]);
+
+            // Score clashes
+            for (k = i + 1; k < timetable.length; k += 1) {
+                for (l = 0; l < timetable[k][0].length; l += 1) {
+                    if (classClash(time, timetable[k][0][l])) {
+                        score += clash;
+                    }
+                }
+            }
+        }
+
+        // Score CBS events
+        if (timetable[i][4] === 'The Bible Talks') {
+            score += tbtClass;
+        } else if (timetable[i][4] === 'Core Theology' || timetable[i][4] === 'Core Training') {
+            score += coreClass;
+        } else if (timetable[i][4] === 'Bible Study') {
+            score += bibleClass;
+        }
+    }
+
+    // Score adjacent times next to CBS events
+
+    // Score free days
+    for (day in uniDays) {
+        if (uniDays.hasOwnProperty(day)) {
+            if (uniDays[day] === false && cbsDays[day] === false) {
+                score += freeScores[day];
+            }
+        }
+    }
+
+    return score;
+}
+*/
