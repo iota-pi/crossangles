16cbd13c6a52ee3e7c734783b19bc72f2915daf3
David <david.adams.16.12@gmail.com>
2017-06-06 23:34:46 +1000

    Now syncs data to server as well

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# NB: gfs-sydney is for testing purposes only!
 cat << EOF | sftp -P 20022 gfssyde3@gfs-sydney.org:public_html/crossangles/
 put -pr js
 put -pr css
+put -pr data
 put index.html
 EOF
