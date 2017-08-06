45aef04a21c8975221cd3b52640caca22004b84f
David <david.adams.16.12@gmail.com>
2017-08-06 22:00:12 +1000

    Fixed leftover debugging

--- js/page.js
+++ js/page.js
@@ -468,7 +468,7 @@ function canSaveTimetable() {
     var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
     var isIE = /*@cc_on!@*/false || !!document.documentMode; // IE 6-11
     var isEdge = !isIE && !!window.StyleMedia;
-    return false && !isSafari && !isEdge && !isIE;
+    return !isSafari && !isEdge && !isIE;
     //jshint ignore:end
 }
 
