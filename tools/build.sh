70a6ce821bac5322ec1f1da69e9847d3276deed6
David <david.adams.16.12@gmail.com>
2017-09-13 23:05:57 +1000

    Dist now builds compiled and compressed CSS & JS

--- tools/build.sh
+++ tools/build.sh
@@ -1,16 +1,59 @@
 #!/bin/bash
 # Builds CrossAngles to dist/
+#
 # Assumptions:
 #  - No <style> tags in index.html; otherwise output will be in strange order (use <link>s instead)
+#  - Stylesheet <link>s are contained in a <noscript> tag
 #
 # Install dependencies: (from inside project root directory)
-#  - sudo npm install grunt-cli clean-css-cli html-minifier -g
+#  - sudo npm install grunt-cli clean-css-cli uglify-js html-minifier -g
 #  - npm install grunt grunt-critical
 #
 # Running:
 #  - Run from project root directory
 #  - './build'
 #
+# Written by David
+#
+
+
+echo -n "Copying files to 'dist/'... "
+# Clear dist initially
+touch index.html # makes sure 'dist/*' gets expanded to *something*
+rm -r dist/*
+mkdir dist/css dist/js dist/data dist/img
+
+# Copy minified CSS/JS files
+cp css/*.min.css dist/css/
+cp js/*.min.js dist/js/
+
+# Copy timetable data file
+cp data/timetable.json dist/data/
+
+# Copy fonts and images
+cp -r fonts/ dist/
+cp -r img/ dist/
+
+# Copy favicon
+cp favicon.png dist/
+
+# Copy scraper
+cp favicon.png dist/
+echo 'Done'
+
+
+
+# Concat and minify all CSS
+# NB: have to cat files in the right order
+echo -n "Creating 'all.css' and 'all.js'... "
+# CSS
+cat `grep -o 'css/.*\.css' index.html` | cleancss -O2 css/*.min.css -o dist/css/all.css
+# JS
+cat `grep -o 'js/.*\.js' index.html` |
+uglifyjs -o dist/js/all.js --keep-fnames
+echo 'Done'
+
+
 
 # Run grunt to get critical CSS
 echo -n 'Running grunt... '
@@ -74,37 +117,30 @@ echo -n 'Minifying HTML file with critical CSS... '
 sed -n '1,/<style/p' 'dist/index.html' >'dist/index.min.html'
 
 # Minify CSS
-#! sudo npm install clean-css-cli -g
 cleancss -O2 <tmp >>'dist/index.min.html'
 rm tmp
 
 # Put everything after </style> line
 sed -n '/<\/style>/,$p' 'dist/index.html' >>'dist/index.min.html'
 
+# Replace CSS files with 'all.css'
+grep -v '<link.*rel="stylesheet"' >tmp <dist/index.min.html
+sed -i 's|\(<noscript.*\)|\1<link rel="stylesheet" type="text/css" href="css/all.css">|' tmp
+mv tmp dist/index.min.html
+
+# Replace JS files with 'all.js'
+grep -v '<script.*src="' >tmp <dist/index.min.html
+sed -i 's|</body>|<script type="text/javascript" src="js/all.js" defer></script></body>|' tmp
+mv tmp dist/index.min.html
+
 # Minify HTML
-#! sudo npm install html-minifier -g
 html-minifier 'dist/index.min.html' -o 'dist/index.html' --collapse-whitespace --html5 --remove-comments --remove-script-type-attributes --remove-style-link-type-attributes --minify-js
 rm 'dist/index.min.html'
 echo 'Done'
 
-# Copy minified CSS/JS files
-echo -n 'Copying files to dist/... '
-cp css/*.min.css dist/css/
-cp js/*.min.js dist/js/
 
-# Copy timetable data file
-cp data/timetable.json dist/data/
 
-# Copy fonts and images
-cp -r fonts/ dist/fonts/
-cp -r img/ dist/img/
-
-# Copy favicon
-cp favicon.png dist/
-
-# Copy scraper
-cp favicon.png dist/
-echo 'Done'
+# All finished!
 echo
 echo 'Build complete'
 
