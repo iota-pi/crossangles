01ffe11fe87eeca4ddef15f6ebe9ce94a655d07b
David <david.adams.16.12@gmail.com>
2017-09-18 08:01:45 +1000

    Add email success/error feedback

--- contact.php
+++ contact.php
@@ -2,6 +2,7 @@
 /* Simple script to send email from bug report form
  */
 
+$result = array();
 $data = json_decode(file_get_contents('php://input'), true);
 $name = filter_var($data['name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $email = filter_var($data['email'], FILTER_SANITIZE_EMAIL);
@@ -9,7 +10,12 @@ $body = filter_var($data['body'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) .
     "\r\n\r\nAutomated email send from CrossAngles.\r\nSender name: $name\r\n";
 if ($email && $body && filter_var($email, FILTER_VALIDATE_EMAIL)) {
     // Important that email has been sanitised
-    mail('crossangles@my.campusbiblestudy.org', 'CrossAngles Contact Form', $body, 'From: no-reply@my.campusbiblestudy.com' . "\r\n" . 'Reply-To: ' . $email);
+    $success = mail('crossangles@my.campusbiblestudy.org', 'CrossAngles Contact Form', $body, 'From: no-reply@my.campusbiblestudy.com' . "\r\n" . 'Reply-To: ' . $email);
+    $result['success'] = $success;
+} else {
+    $result['success'] = false;
+    $result['error'] = 'Invalid email address';
 }
+echo json_encode($result);
 
 ?>
