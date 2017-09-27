6b3bb1095daa526a00cdbd878d41e4902fda73f9
David <david.adams.16.12@gmail.com>
2017-09-27 13:33:32 +1000

    Minor update to contact email

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <?php
-/* Simple script to send email from bug report form
+/* Simple script to send email from contact form
  */
 
 $result = array();
@@ -7,7 +7,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 $name = filter_var($data['name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $email = filter_var($data['email'], FILTER_SANITIZE_EMAIL);
 $body = filter_var($data['body'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) .
-    "\r\n\r\nAutomated email send from CrossAngles.\r\nSender name: $name\r\n";
+    "\r\n\r\n------\r\nThis is an automated email sent from CrossAngles.\r\nSender name: $name\r\n";
 if ($email && $body && filter_var($email, FILTER_VALIDATE_EMAIL)) {
     // Important that email has been sanitised
     $success = mail('crossangles@my.campusbiblestudy.org', 'CrossAngles Contact Form', $body, 'From: no-reply@my.campusbiblestudy.com' . "\r\n" . 'Reply-To: ' . $email);
