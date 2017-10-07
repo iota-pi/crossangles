3aa29f680851cdc81f11bb898fede3bcd18943ae
iota-pi <david.adams.16.12@gmail.com>
2017-10-07 13:55:18 +1100

    New indexing method for scraper

--- scraper.py
+++ scraper.py
@@ -4,17 +4,21 @@ from lxml import html, etree
 from collections import defaultdict
 import grequests
 import json
+import time
+import os
 import re
 
 class Scraper:
-    def __init__(self, sem):
+    def __init__(self, sem, year):
         self.removeADFA = True
         SEM_CODES = { 'S1': 2, 'S2': 3 }
         self.semester = sem
+        self.year = year
         self.semID = str(SEM_CODES[self.semester])
+        self.data = None
 
     def scrape(self):
-        data = defaultdict(dict)
+        self.data = defaultdict(dict)
         for page in self.loadFacultyPages():
             # Get relevant table
             table = page.xpath('//table[3]')[0]
@@ -32,7 +36,7 @@ class Scraper:
                     code = tr[0].xpath('.//text()')[0].strip()
                     faculty = code[:4]
                     course = code[4:]
-                    data[faculty][course] = [tr[1].xpath('.//text()')[0].strip()]
+                    self.data[faculty][course] = [tr[1].xpath('.//text()')[0].strip()]
                     continue
                 
                 # If we haven't started on a course yet
@@ -41,30 +45,19 @@ class Scraper:
                 
                 # Scrape class info
                 component = tr[0].text
-                status = tr[4].text[0]
+                status = tr[4].text
                 
-                if status not in ['O', 'F']:
+                if status[0] not in ['O', 'F']:
                     continue
-                status = int(status.replace('O', '0').replace('F', '1'))
+                status = int(status[0].replace('O', '0').replace('F', '1'))
                 
-                enrol = tr[5].text.split('/')
+                enrol = map(int, tr[5].text.split('/'))
                 ttdata = self.parseTimeStr(tr[7].text)
-                data[faculty][course].append([component, status, *enrol, *ttdata])
+                self.data[faculty][course].append([component, status, *enrol, *ttdata])
         
-        with open('data/tt.json', 'w') as f:
-            json.dump(data, f, separators=(',', ':'))
-    
-    def loadFacultyPages(self):
-        page = self.loadPages('http://classutil.unsw.edu.au/')[0]
-        links = page.xpath('//td[' + self.semID + '][@class="data"]/a[contains(@href,".html")]/@href')
-        
-        # Filter out ADFA courses
-        if self.removeADFA:
-            links = [link for link in links if link[0] != 'Z']
-        
-        links = links[:10] # Testing only!!
-        
-        return self.loadPages(*links, prefix='http://classutil.unsw.edu.au/')
+        self.index()
+        self.saveJSON()
+
     
     def parseTimeStr(self, string):
         # Remove /odd and /even, as well as Comb/w descriptors
@@ -105,6 +98,45 @@ class Scraper:
 
         return [time, location]
 
+    # Replace components, times and labs with indexes
+    def index(self):
+        components = defaultdict(int)
+        times      = defaultdict(int)
+        locations  = defaultdict(int)
+        for faculty in self.data.keys():
+            for courseData in self.data[faculty].values():
+                for courseClass in courseData[1:]:
+                    components[courseClass[0]] += 1
+                    for time in courseClass[4::2]:
+                        times[time] += 1
+                    for location in courseClass[5::2]:
+                        locations[location] += 1
+
+        # Sort these hashes into lists in descending order of # of uses
+        self.components = self.sortHash(components)
+        self.times = self.sortHash(times)
+        self.locations = self.sortHash(locations)
+
+        # Use indexes into these lists in the main data structure
+        for faculty in self.data.keys():
+            for courseCode, courseData in self.data[faculty].items():
+                for i in range(1, len(courseData)):
+                    courseClass = courseData[i]
+                    self.data[faculty][courseCode][i][0] = self.components.index(courseData[i][0])
+
+                    for j in range(4, len(courseClass), 2):
+                        time = courseClass[j]
+                        self.data[faculty][courseCode][i][j] = self.times.index(time)
+
+                    for j in range(5, len(courseClass), 2):
+                        location = courseClass[j]
+                        self.data[faculty][courseCode][i][j] = self.locations.index(location)
+
+    def sortHash(self, index):
+        index = sorted(index.items(), key=lambda x: x[1], reverse=True)
+        index = list(map(lambda x: x[0], index))
+        return index
+
     def subtime(self, timestr):
         # If the class runs on Saturday or Sunday, don't include it
         if timestr[0].lower() == 's':
@@ -140,6 +172,18 @@ class Scraper:
 
         return True
     
+    def loadFacultyPages(self):
+        page = self.loadPages('http://classutil.unsw.edu.au/')[0]
+        links = page.xpath('//td[' + self.semID + '][@class="data"]/a[contains(@href,".html")]/@href')
+
+        # Filter out ADFA courses
+        if self.removeADFA:
+            links = [link for link in links if link[0] != 'Z']
+
+        links = links[:10] # Testing only!!
+
+        return self.loadPages(*links, prefix='http://classutil.unsw.edu.au/')
+
     def loadPages(self, *args, **kwargs):
         if 'prefix' in kwargs:
             urls = (kwargs['prefix'] + arg for arg in args)
@@ -171,7 +215,35 @@ class Scraper:
 
         return trees
 
+    #
+    # getSydneyTime(): returns the date (dd/mm/yy) and time (hh:mm) in Sydney
+    #  (this is particularly useful for when script is being run on a webserver in another location)
+    #
+    def getSydneyTime(self):
+        now = time.time()
+        os.environ['TZ'] = 'Australia/Sydney' # Force Sydney timezone
+        thedate = time.strftime('%d/%m/%Y', time.localtime(now))
+        thetime = time.strftime('%H:%M', time.localtime(now))
+        return thedate, thetime
+
+    def getMeta(self):
+        update_date, update_time = self.getSydneyTime()
+        self.meta = {
+            'sem': self.semester,
+            'year': self.year,
+            'updated': update_date,
+            'uptimed': update_time
+        }
+
+    def saveJSON(self):
+        self.getMeta()
+        allData = [self.data, self.components, self.locations, self.times, self.meta]
+
+        with open('data/tt.json', 'w') as f:
+            # Write file with maximum compression
+            json.dump(allData, f, separators=(',', ':'))
+
 
 if __name__ == '__main__':
-    sc = Scraper('S1')
+    sc = Scraper('S1', 2018)
     sc.scrape()
