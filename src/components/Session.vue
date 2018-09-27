30ef53c456523248fe680c7fade6f7f78b82a56a
David Adams <david.adams.16.12@gmail.com>
2018-09-27 20:12:11 +1000

    Add initial session code

---
+++ src/components/Session.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    class="session"
+    @mousedown="drag"
+    @mouseup="drop"
+  >
+    <div class="title">
+      {{ title }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        dragging: false
+      }
+    },
+    computed: {
+      title () {
+        return 'Test Title'
+      }
+    },
+    methods: {
+      drag (e) {
+        console.log('drag', e)
+        this.dragging = true
+      },
+      drop (e) {
+        console.log('drop', e)
+        this.dragging = false
+      }
+    },
+    props: {
+
+    }
+  }
+</script>
+
+<style scoped>
+  .session {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background-color: #ff00ff;
+    color: white;
+    cursor: grab;
+  }
+  .session.dragging {
+    cursor: grabbing;
+  }
+</style>
