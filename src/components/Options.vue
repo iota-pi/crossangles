3d100d57c23af369071add731621e1877b703f1a
David Adams <david.adams.16.12@gmail.com>
2018-10-14 09:14:20 +1100

    Use object rather than array for options

--- src/components/Options.vue
+++ src/components/Options.vue
@@ -3,7 +3,7 @@
     <v-flex xs12 sm4 v-for="option in allOptions" :key="option.title">
       <v-checkbox
         v-model="options"
-        :value="option"
+        :value="option.value"
         :label="option.title"
         color="secondary"
         class="mt-2"
@@ -21,20 +21,24 @@
         options: [],
         allOptions: [
           {
-            title: 'Show class locations'
+            title: 'Show class locations',
+            value: { 'locations': true }
           },
           {
-            title: 'Show class enrolments'
+            title: 'Show class enrolments',
+            value: { 'enrolments': true }
           },
           {
-            title: 'Include full classes'
+            title: 'Include full classes',
+            value: { 'allowFull': true }
           }
         ]
       }
     },
     watch: {
       options () {
-        this.$store.commit('options', this.options)
+        let asObject = Object.assign({}, ...this.options)
+        this.$store.commit('options', asObject)
       }
     }
   }
