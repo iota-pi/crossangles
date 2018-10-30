ed3b65ad4a4c49b7aa7506af9e67bb34d489e125
David Adams <david.adams.16.12@gmail.com>
2018-10-30 20:52:27 +1100

    Combine both email checks into the same place

--- php/contact.php
+++ php/contact.php
@@ -9,7 +9,7 @@ $email = substr(filter_var($data['email'], FILTER_SANITIZE_EMAIL), 0, 255);
 $body = substr(filter_var($data['body'], FILTER_SANITIZE_FULL_SPECIAL_CHARS), 0, 5000) .
         "\r\n\r\n------\r\nThis is an automated email sent from CrossAngles.\r\n" .
         "Sender name: $name\r\n";
-if (!$email) {
+if (!$email || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
   http_response_code(400);
   $result['success'] = false;
   $result['error'] = 'Invalid email address!';
@@ -17,7 +17,7 @@ if (!$email) {
   http_response_code(400);
   $result['success'] = false;
   $result['error'] = 'Message should not be empty';
-} else if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+} else {
   $success = mail('crossangles@my.campusbiblestudy.org', 'CrossAngles Contact Form', $body, 'From: no-reply@my.campusbiblestudy.com' . "\r\n" . 'Reply-To: ' . $email);
   if ($success) {
     $result['success'] = true;
