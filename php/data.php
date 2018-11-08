51958ce422bc79da17730360cae2d6abf55ddc27
David Adams <david.adams.16.12@gmail.com>
2018-11-09 08:16:01 +1100

    Add JSON content-type header for timetable data

--- php/data.php
+++ php/data.php
@@ -2,6 +2,7 @@
 /* Proxy request to get timetable data */
 
 $url = json_decode(file_get_contents('./config/dataURI.json'));
+header('Content-Type: application/json');
 echo file_get_contents($url);
 
 ?>
