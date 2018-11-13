63ae17762b4eb17f5c9f1e2350b5d9da5b942036
David Adams <david.adams.16.12@gmail.com>
2018-11-13 17:54:28 +1100

    Proper PWA config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  productionSourceMap: false
+  productionSourceMap: false,
+
+  pwa: {
+    name: 'CrossAngles',
+    themeColor: '#303F9F',
+    msTileColor: '#2d89ef',
+    appleMobileWebAppCapable: 'yes'
+  }
 }
