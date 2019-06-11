b62865efd5a044812091fe502168fee4418974df
David Adams <david.adams.16.12@gmail.com>
2019-06-11 14:04:02 +1000

    Use secondary color for 'Save as Image' button

--- src/components/SaveTimetable.vue
+++ src/components/SaveTimetable.vue
@@ -3,14 +3,13 @@
     outline
     round
     large
-    color="primary"
+    color="secondary"
     class="fixed-width"
     @click="$emit('save')"
   >
     <span v-if="!saving">Save as Image</span>
     <v-progress-circular
       v-else
-      color="white"
       indeterminate
       :size="24"
       :width="3"
