990d331b7bff13ce7e6e2dfb175d6f42a77a22a1
David Adams <david.adams.16.12@gmail.com>
2019-09-12 08:35:19 +1000

    Store timetable with consistent snapToggle

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -3,12 +3,12 @@ import thunk from 'redux-thunk';
 import { persistStore, persistReducer } from 'redux-persist';
 import reducer from './reducers';
 import storage from 'redux-persist/lib/storage';
-import { coursesTransform, coloursTransform } from './state/transforms';
+import { coursesTransform, coloursTransform, timetableTransform } from './state/transforms';
 
 const persistConfig = {
   key: 'root',
   storage,
-  transforms: [ coursesTransform, coloursTransform ],
+  transforms: [ coursesTransform, coloursTransform, timetableTransform ],
 }
 const persistedReducer = persistReducer(persistConfig, reducer);
 export const store = createStore(persistedReducer, applyMiddleware(thunk));
