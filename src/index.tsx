e72b8c4a0718eef95680348332668c80a1b47480
David Adams <david.adams.16.12@gmail.com>
2020-02-04 21:10:37 +1100

    Use react-router

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { store, persistor } from './configureStore';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
+
+import loadable from '@loadable/component';
+const App = loadable(() => import('./App'));
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate persistor={persistor} loading={null}>    {/* TODO: add pretty loading element */}
-      <App />
+    <PersistGate persistor={persistor} loading={null}>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <App />
+          </Route>
+        </Switch>
+      </Router>
     </PersistGate>
   </Provider>,
   document.getElementById('root')
