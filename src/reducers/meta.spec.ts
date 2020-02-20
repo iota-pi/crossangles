7908408c3865c5320ff00f7d0aa8158ebbb160bf
David Adams <david.adams.16.12@gmail.com>
2020-02-21 08:58:11 +1100

    Add tests for meta reducer

---
+++ src/reducers/meta.spec.ts
@@ -0,0 +1,43 @@
+import { meta } from './meta';
+import { ClearNoticeAction, CLEAR_NOTICE, CourseAction, TOGGLE_WEB_STREAM, SET_META_DATA, MetaAction } from '../actions';
+import { baseState } from '../state';
+import { CourseId } from '../state/Course';
+import { Meta } from '../state/Meta';
+
+const otherAction: ClearNoticeAction = { type: CLEAR_NOTICE };
+
+describe('meta reducer', () => {
+  it('initialises correctly', () => {
+    const state = meta(undefined, otherAction);
+    expect(state).toEqual(baseState.meta);
+  })
+
+  it('doesn\'t change on no-op actions', () => {
+    const state = { ...baseState.meta };
+    const result = meta(state, otherAction);
+    expect(result).toBe(state);
+    expect(state).toEqual(baseState.meta);
+  })
+
+  it('can be set', () => {
+    const testMeta = { ...baseState.meta };
+    const newMeta: Meta = {
+      linkText: 'abc',
+      linkURL: 'abc',
+      promoText: 'abc',
+      signup: '',
+      source: '',
+      term: 1,
+      updateDate: '',
+      updateTime: '',
+      year: 1984,
+    };
+    const action: MetaAction = {
+      type: SET_META_DATA,
+      meta: newMeta,
+    };
+    const state = meta(testMeta, action);
+    expect(testMeta).toEqual(baseState.meta);
+    expect(state).toEqual(newMeta);
+  })
+})
