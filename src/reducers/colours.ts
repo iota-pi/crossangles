a490e3a2ad44c18689967960811687777ab5ad1b
David Adams <david.adams.16.12@gmail.com>
2020-02-20 21:28:22 +1100

    Pick non-duplicate colours for autoSelect courses

--- src/reducers/colours.ts
+++ src/reducers/colours.ts
@@ -8,24 +8,27 @@ export function colours (state = baseState.colours, action: AllActions): ColourM
   switch (action.type) {
     case ADD_COURSE:
       const courseId = getCourseId(action.course);
-      const colour = action.course.defaultColour || pickColor(chosenColours);
+      const colour = action.course.defaultColour || pickColour(chosenColours);
       return {
         ...state,
         [courseId]: colour,
       };
     case SET_COLOUR:
-      const newColour = action.colour ? action.colour : pickColor(chosenColours);
+      const newColour = action.colour ? action.colour : pickColour(chosenColours);
       return {
         ...state,
         [action.course]: newColour,
       };
     case SET_COURSE_DATA:
       const additional = action.courses.filter(c => c.isAdditional && c.autoSelect);
+      const colourPool = [...COURSE_COLOURS];
       state = Object.assign(
         {},
-        ...additional.map(c => ({
-          [getCourseId(c)]: c.defaultColour || pickColor(chosenColours),
-        })),
+        ...additional.map(c => {
+          const colour = c.defaultColour || pickColour(chosenColours, colourPool);
+          colourPool.splice(colourPool.indexOf(colour), 1);
+          return { [getCourseId(c)]: colour };
+        }),
         state,
       );
 
@@ -35,12 +38,16 @@ export function colours (state = baseState.colours, action: AllActions): ColourM
   return state;
 };
 
-function pickColor (chosenColours: Colour[]): Colour {
-  let canChoose = COURSE_COLOURS.filter(c => !chosenColours.includes(c));
+function pickColour (chosenColours: Colour[], colourPool: Colour[] = COURSE_COLOURS): Colour {
+  // Prefer to choose any colours which haven't been chosen yet
+  let canChoose = colourPool.filter(c => !chosenColours.includes(c));
+
+  // All colours have been chosen at least once, so just pick anything
   if (canChoose.length === 0) {
-    canChoose = COURSE_COLOURS;
+    canChoose = colourPool;
   }
 
   const i = Math.floor(Math.random() * canChoose.length);
-  return canChoose[i];
+  const colour = canChoose[i];
+  return colour;
 }
