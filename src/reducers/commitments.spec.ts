d4600d6b1cfc11d53a31b0eb6cebdbb9f1170b15
David Adams <david.adams.16.12@gmail.com>
2020-02-21 19:35:17 +1100

    Add unit tests for options reducer

--- src/reducers/commitments.spec.ts
+++ src/reducers/commitments.spec.ts
@@ -1,5 +1,5 @@
-import { events } from './commitments';
-import { ClearNoticeAction, CLEAR_NOTICE, EventAction, TOGGLE_EVENT, CourseAction, ADD_COURSE } from '../actions';
+import { events, options, hiddenEvents } from './commitments';
+import { ClearNoticeAction, CLEAR_NOTICE, EventAction, TOGGLE_EVENT, CourseAction, ADD_COURSE, ToggleOptionAction, TOGGLE_OPTION } from '../actions';
 import { baseState } from '../state';
 
 const otherAction: ClearNoticeAction = { type: CLEAR_NOTICE };
@@ -124,5 +124,48 @@ describe('events reducer', () => {
     };
     const result = events(state, action);
     expect(result).toEqual([]);
+  })
+})
+
+describe('options reducer', () => {
+  it('initialises correctly', () => {
+    const state = options(undefined, otherAction);
+    expect(state).toEqual(baseState.options);
+  })
+
+  it('doesn\'t change on no-op actions', () => {
+    const state = Object.freeze({ ...baseState.options });
+    const result = options(state, otherAction);
+    expect(result).toBe(state);
+  })
+
+  it('allows toggling options on from undefined', () => {
+    const state = Object.freeze({});
+    const action: ToggleOptionAction = {
+      type: TOGGLE_OPTION,
+      option: 'includeFull',
+    };
+    const result = options(state, action);
+    expect(result['includeFull']).toBe(true);
+  })
+
+  it('allows toggling options on from false', () => {
+    const state = Object.freeze({'includeFull': false});
+    const action: ToggleOptionAction = {
+      type: TOGGLE_OPTION,
+      option: 'includeFull',
+    };
+    const result = options(state, action);
+    expect(result['includeFull']).toBe(true);
+  })
+
+  it('allows toggling options off', () => {
+    const state = Object.freeze({'includeFull': true});
+    const action: ToggleOptionAction = {
+      type: TOGGLE_OPTION,
+      option: 'includeFull',
+    };
+    const result = options(state, action);
+    expect(result['includeFull']).toEqual(false);
   })
 })
