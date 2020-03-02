526a4814dc03f37e34732b714ffee1dc8b62bb44
David Adams <david.adams.16.12@gmail.com>
2020-03-02 19:59:45 +1100

    Add save as image button

--- src/components/ActionButtons.tsx
+++ src/components/ActionButtons.tsx
@@ -3,19 +3,33 @@ import Button from '@material-ui/core/Button';
 import { Meta } from '../state/Meta';
 import { Theme, createStyles, WithStyles, withStyles } from '@material-ui/core/styles';
 import OpenInNewIcon from '@material-ui/icons/OpenInNew';
+import CameraAltIcon from '@material-ui/icons/CameraAlt';
 
 const styles = (theme: Theme) => createStyles({
   root: {
     display: 'flex',
     justifyContent: 'center',
   },
-  roundedButton: {
-    borderColor: theme.palette.grey[700],
+  buttonHolder: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  baseButton: {
+    flexGrow: 1,
+    flexBasis: 0,
     borderRadius: 26,
+    minHeight: 52,
+    marginBottom: theme.spacing(2),
+  },
+  signUpButton: {
+    borderColor: theme.palette.grey[700],
     lineHeight: 1.35,
     paddingTop: 5,
     paddingBottom: 5,
     textTransform: 'none',
+  },
+  saveImageButton: {
+
   },
   fontNormal: {
     fontWeight: 500,
@@ -48,25 +62,37 @@ export const ActionButtons = withStyles(styles)(({
 
   return (
     <div className={rootClasses}>
-      {showSignup && (
+      <div className={classes.buttonHolder}>
         <Button
           variant="outlined"
-          className={classes.roundedButton}
+          className={classes.baseButton + ' ' + classes.saveImageButton}
           size="large"
-          href={meta.signupURL}
-          endIcon={<OpenInNewIcon />}
-          target="_blank"
+          color="primary"
+          endIcon={<CameraAltIcon />}
         >
-          <div className={classes.centredText}>
-            <div className={classes.fontNormal}>
-              Sign Up for Term {meta.term}
-            </div>
-            <div className={classes.fontLight}>
-              {meta.ministryName}
-            </div>
-          </div>
+          Save as Image
         </Button>
-      )}
+
+        {showSignup && (
+          <Button
+            variant="outlined"
+            className={classes.baseButton + ' ' + classes.signUpButton}
+            size="large"
+            endIcon={<OpenInNewIcon />}
+            href={meta.signupURL}
+            target="_blank"
+          >
+            <div className={classes.centredText}>
+              <div className={classes.fontNormal}>
+                Sign Up for Term {meta.term}
+              </div>
+              <div className={classes.fontLight}>
+                {meta.ministryName}
+              </div>
+            </div>
+          </Button>
+        )}
+      </div>
     </div>
   )
 });
