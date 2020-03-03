06408a7c188bb6f70456ef426cb25779cf176413
David Adams <david.adams.16.12@gmail.com>
2020-03-03 21:26:10 +1100

    Lift ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+import { ThemeProvider } from '@material-ui/core';
+import theme from './theme';
 
 import loadable from '@loadable/component';
 const App = loadable(() => import('./App'));
+const StandaloneTimetable = loadable(() => import('./containers/StandaloneTimetable'));
 
 ReactDOM.render(
+  <ThemeProvider theme={theme}>
   <Router>
     <Switch>
       <Route path="/">
         <App />
       </Route>
     </Switch>
-  </Router>,
+    </Router>
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
