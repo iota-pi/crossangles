96562b268d235cad6550e96155f7dc91dc0d6fc7
David Adams <david.adams.16.12@gmail.com>
2020-03-16 20:14:27 +1100

    Add FooterImage component

---
+++ src/components/FooterImage.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import getCampus from '../getCampus';
+import { WithStyles, createStyles, Theme, withStyles } from '@material-ui/core/styles';
+
+
+const styles = (theme: Theme) => createStyles({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: theme.spacing(12),
+  },
+  image: {
+    width: 150,
+    height: 150,
+  },
+});
+
+export interface Props extends WithStyles<typeof styles> {}
+
+
+export const FooterImage = withStyles(styles)(({ classes }: Props) => {
+  const campus = getCampus(window.location.hostname);
+  const images: { [campus: string]: string } = {
+    unsw: 'img/cbs.png',
+  };
+  const src = images[campus];
+
+  return (
+    <div className={classes.root}>
+      <img src={src} alt="" className={classes.image} />
+    </div>
+  );
+});
