e5063d5b0e32bdffa2409f87c3ac566438305dec
David Adams <david.adams.16.12@gmail.com>
2020-04-16 21:01:31 +1000

    Remove tooltip

--- app/src/components/CourseDisplay.tsx
+++ app/src/components/CourseDisplay.tsx
@@ -232,33 +232,23 @@ export const AdditionalCourseDisplay = withStyles(styles)(({
     rootClasses.push(classes.minimised);
   }
 
-  const courseLabel = course.metadata ? (
-    <a
-      href={course.metadata.website}
-      target="_blank"
-      rel="noopener noreferrer"
-      className={classes.plainLink}
-    >
-      <span>{course.name}</span>
-      <OpenInNew className={classes.externalLinkIcon} fontSize={'inherit'} />
-    </a>
-  ) : (
-    <span>{course.name}</span>
-  );
-
   return (
     <React.Fragment>
       <ListItem className={rootClasses.join(' ')}>
         <ListItemText>
-          {course.description ? (
-            <Tooltip
-              title={course.description}
-              aria-hidden="true"
-              // placement="right"
+          {course.metadata ? (
+            <a
+              href={course.metadata.website}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={classes.plainLink}
             >
-              {courseLabel}
-            </Tooltip>
-          ) : courseLabel}
+              <span>{course.name}</span>
+              <OpenInNew className={classes.externalLinkIcon} fontSize={'inherit'} />
+            </a>
+          ) : (
+            <span>{course.name}</span>
+          )}
         </ListItemText>
 
         <div className={classes.marginRight}>
