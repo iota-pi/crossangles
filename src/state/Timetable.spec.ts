09e7e550f2f6fddbee77c4c5b6c86e66d6f51a6a
David Adams <david.adams.16.12@gmail.com>
2020-04-26 09:05:09 +1000

    Unit tests for getCurrentTimetable/Term

---
+++ src/state/Timetable.spec.ts
@@ -0,0 +1,35 @@
+import { getCurrentTimetable } from './Timetable';
+import { initialState } from '.';
+import SessionManager, { SessionManagerData } from '../components/Timetable/SessionManager';
+
+
+describe('getCurrentTimetable', () => {
+  it('gives the correct value', () => {
+    const timetable: SessionManagerData = {
+      map: [],
+      order: [],
+      score: 0,
+      version: 0,
+    }
+    const state = {
+      timetables: {
+        '2020~2': timetable,
+      },
+      meta: {
+        ...initialState.meta,
+        year: 2020,
+        term: 2,
+      },
+    };
+    expect(getCurrentTimetable(state)).toBe(timetable);
+    expect(getCurrentTimetable(state)).toBe(timetable);
+  });
+
+  it('defaults to an empty timetable', () => {
+    const state = {
+      timetables: {},
+      meta: initialState.meta,
+    };
+    expect(getCurrentTimetable(state)).toEqual(new SessionManager().data);
+  });
+});
