1a3b7e2c798a021b6e9d33ffd401ba14dfe8eff1
David Adams <david.adams.16.12@gmail.com>
2020-05-02 09:40:24 +1000

    Fewer double quotes

--- app/src/campusConfig.ts
+++ app/src/campusConfig.ts
@@ -11,10 +11,10 @@ export interface CampusConfig {
 export const dataDomain = 'https://d1aa8o4hf8f3x4.cloudfront.net/';
 
 export const campusConfig: AllCampusConfig = {
-  "unsw": {
-    dataPath: "unsw/data.json",
-    name: "UNSW",
-    longname: "University of New South Wales",
+  unsw: {
+    dataPath: 'unsw/data.json',
+    name: 'UNSW',
+    longname: 'University of New South Wales',
   },
 };
 campusConfig.next = campusConfig.unsw;
