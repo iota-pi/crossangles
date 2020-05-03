49bbab0172fe6d77748edf2b7e64db8d0387b91a
David Adams <david.adams.16.12@gmail.com>
2020-05-03 21:41:59 +1000

    Additional reducer tests

--- src/reducers/courses.spec.ts
+++ src/reducers/courses.spec.ts
@@ -1,8 +1,137 @@
-import { additional, custom, chosen } from './courses';
+import { additional, custom, chosen, courses } from './courses';
 import { initialState } from '../state';
-import { CourseListAction, CourseAction, REMOVE_COURSE, SET_COURSE_DATA, CustomAction, ADD_CUSTOM, REMOVE_CUSTOM, ADD_COURSE } from '../actions';
-import { CourseData, getCourseId } from '../state/Course';
+import { CourseListAction, CourseAction, REMOVE_COURSE, SET_COURSE_DATA, CustomAction, ADD_CUSTOM, REMOVE_CUSTOM, ADD_COURSE, UPDATE_CUSTOM } from '../actions';
+import { CourseData, getCourseId, CourseMap } from '../state/Course';
 import { getCourse } from '../test_util';
+
+
+describe('courses', () => {
+  it('initialises correctly', () => {
+    expect(courses(undefined, { type: '@@INIT' } as any)).toEqual(initialState.courses);
+  })
+
+  it('returns same object for no-ops', () => {
+    const state = { ...initialState.courses };
+    const result = courses(state, { type: 'NO_OP_ACTION' } as any);
+    expect(result).toBe(state);
+    expect(result).toEqual(initialState.courses);
+  })
+
+  it('sets course data when blank', () => {
+    const courseList: CourseData[] = [
+      { code: 'RING1379', name: 'Ring Theory 1A', streams: [] },
+      { code: 'RING9731', name: 'Introduction to Ring Theory', streams: [] },
+    ];
+    const action: CourseListAction = {
+      type: SET_COURSE_DATA,
+      courses: courseList,
+    };
+    const state = {};
+    const result = courses(state, action);
+    expect(result).toEqual({
+      [getCourseId(courseList[0])]: courseList[0],
+      [getCourseId(courseList[1])]: courseList[1],
+    });
+  })
+
+  it('merges new course data with existing data', () => {
+    const courseList: CourseData[] = [
+      { code: 'RING1379', name: 'Ring Theory 1A', streams: [] },
+      { code: 'RING9731', name: 'Introduction to Ring Theory', streams: [] },
+    ];
+    const action: CourseListAction = {
+      type: SET_COURSE_DATA,
+      courses: courseList,
+    };
+    const state: CourseMap = {
+      [getCourseId(courseList[0])]: { code: 'RING1379', name: 'Old Ring Theory Name', streams: [] },
+      'observer': { code: 'observer', name: 'Observation', streams: [] },
+    };
+    const result = courses(state, action);
+    expect(result).toEqual({
+      ...state,
+      [getCourseId(courseList[0])]: courseList[0],
+      [getCourseId(courseList[1])]: courseList[1],
+    });
+    expect(result[getCourseId(courseList[0])].name).toBe(courseList[0].name);
+  })
+
+  it('handles new custom course', () => {
+    const course: CourseData = {
+      ...getCourse(),
+      isCustom: true,
+    };
+    const action: CustomAction = {
+      type: ADD_CUSTOM,
+      custom: course,
+    };
+    const state = {};
+    const result = courses(state, action);
+    expect(result).toEqual({
+      ...state,
+      [getCourseId(course)]: course,
+    });
+  })
+
+  it('handles updating a custom course', () => {
+    const course: CourseData = {
+      ...getCourse(),
+      isCustom: true,
+    };
+    const updatedCourse: CourseData = {
+      ...course,
+      name: 'New Ring Theory Name',
+    };
+    const action: CustomAction = {
+      type: UPDATE_CUSTOM,
+      custom: updatedCourse,
+    };
+    const state = { [getCourseId(course)]: course };
+    const result = courses(state, action);
+    expect(result).toEqual({
+      ...state,
+      [getCourseId(updatedCourse)]: updatedCourse,
+    });
+  })
+
+  it('handles removing only custom course', () => {
+    const course: CourseData = {
+      ...getCourse(),
+      isCustom: true,
+    };
+    const action: CustomAction = {
+      type: REMOVE_CUSTOM,
+      custom: course,
+    };
+    const state = { [getCourseId(course)]: course };
+    const result = courses(state, action);
+    expect(result).toEqual({});
+  })
+
+  it('removing a custom course doesn\'t affect other courses', () => {
+    const course: CourseData = {
+      ...getCourse(),
+      isCustom: true,
+    };
+    const observerCourse: CourseData = {
+      code: 'observer',
+      name: 'observer',
+      streams: [],
+    };
+    const action: CustomAction = {
+      type: REMOVE_CUSTOM,
+      custom: course,
+    };
+    const state = {
+      [getCourseId(course)]: course,
+      [getCourseId(observerCourse)]: observerCourse,
+    };
+    const result = courses(state, action);
+    expect(result).toEqual({
+      [getCourseId(observerCourse)]: observerCourse,
+    });
+  })
+})
 
 
 describe('chosen', () => {
@@ -58,14 +187,14 @@ describe('chosen', () => {
   })
 
   it('removes courses which no longer exist', () => {
-    const courses: CourseData[] = [
+    const courseList: CourseData[] = [
       { code: 'RING1379', name: 'Ring Theory 1A', streams: [] },
       { code: 'RING9731', name: 'Introduction to Ring Theory', streams: [] },
     ];
-    const courseIds = courses.map(c => getCourseId(c));
+    const courseIds = courseList.map(c => getCourseId(c));
     const action: CourseListAction = {
       type: SET_COURSE_DATA,
-      courses,
+      courses: courseList,
     };
     const state = ['extra1', courseIds[0], 'extra3', courseIds[1], 'extra3'];
     const result = chosen(state, action);
@@ -73,14 +202,14 @@ describe('chosen', () => {
   })
 
   it('doesn\'t change if all courses exist', () => {
-    const courses: CourseData[] = [
+    const courseList: CourseData[] = [
       { code: 'RING1379', name: 'Ring Theory 1A', streams: [] },
       { code: 'RING9731', name: 'Introduction to Ring Theory', streams: [] },
     ];
-    const courseIds = courses.map(c => getCourseId(c));
+    const courseIds = courseList.map(c => getCourseId(c));
     const action: CourseListAction = {
       type: SET_COURSE_DATA,
-      courses,
+      courses: courseList,
     };
     const state = [ ...courseIds ];
     const result = chosen(state, action);
