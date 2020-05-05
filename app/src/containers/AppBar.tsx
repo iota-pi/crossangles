4d43ead7aaee069185236012c9d8ba7b930b2c1a
David Adams <david.adams.16.12@gmail.com>
2020-05-05 20:33:21 +1000

    Refactor AppBar to functional component

--- app/src/containers/AppBar.tsx
+++ app/src/containers/AppBar.tsx
@@ -1,6 +1,5 @@
-import React, { PureComponent } from 'react';
-import withStyles, { WithStyles } from "@material-ui/core/styles/withStyles";
-import createStyles from "@material-ui/core/styles/createStyles";
+import React from 'react';
+import createStyles from '@material-ui/core/styles/createStyles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -8,46 +7,51 @@ import IconButton from '@material-ui/core/IconButton';
 import { AboutCrossAngles } from '../components/AboutCrossAngles';
 import InvertColors from '@material-ui/icons/InvertColors';
 import InvertColorsOff from '@material-ui/icons/InvertColorsOff';
+import { makeStyles } from '@material-ui/core';
 
-const styles = () => {
+const useStyles = makeStyles(() => {
   return createStyles({
     grow: {
       flexGrow: 1,
     },
   });
-};
+});
 
-export interface Props extends WithStyles<typeof styles> {
+export interface Props {
   darkMode: boolean,
   onShowContact: () => void,
   onToggleDarkMode: (darkMode?: boolean) => void,
 };
 
 
-class CrossAnglesAppBar extends PureComponent<Props> {
-  render() {
-    const { classes } = this.props;
-    return (
-      <AppBar
-        position="absolute"
-      >
-        <Toolbar>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-            CrossAngles
-          </Typography>
+export function CrossAnglesAppBar({
+  darkMode,
+  onShowContact,
+  onToggleDarkMode,
+}: Props) {
+  const classes = useStyles();
 
-          <IconButton onClick={() => this.props.onToggleDarkMode()} color="inherit">
-            {this.props.darkMode ? (
-              <InvertColorsOff />
-            ) : (
-              <InvertColors />
-            )}
-          </IconButton>
-          <AboutCrossAngles onShowContact={this.props.onShowContact} />
-        </Toolbar>
-      </AppBar>
-    );
-  }
-};
+  return (
+    <AppBar
+      position="absolute"
+    >
+      <Toolbar>
+        <Typography variant="h6" color="inherit" className={classes.grow}>
+          CrossAngles
+        </Typography>
 
-export default withStyles(styles)(CrossAnglesAppBar);
+        <IconButton onClick={() => onToggleDarkMode()} color="inherit">
+          {darkMode ? (
+            <InvertColorsOff />
+          ) : (
+            <InvertColors />
+          )}
+        </IconButton>
+
+        <AboutCrossAngles onShowContact={onShowContact} />
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default CrossAnglesAppBar;
