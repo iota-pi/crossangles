170feabb21b01753c64ec9838e16501e228ccf30
David Adams <david.adams.16.12@gmail.com>
2020-05-14 08:48:19 +1000

    Add app deployment infra

---
+++ infra/app/main.tf
@@ -0,0 +1,78 @@
+# TODO: dns
+
+locals {
+  origin_id = "app_s3_origin"
+}
+
+resource "aws_s3_bucket" "app" {
+  bucket = "crossangles-app-${var.environment}"
+  acl    = "private"
+
+  tags = {
+    Name        = "CrossAngles Data"
+    Environment = "${var.environment}"
+  }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = ["*"]
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "app_oai" {}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.app.bucket_regional_domain_name
+    origin_id   = local.origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.app_oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.origin_id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 1800
+    max_ttl                = 3600
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+output "cloudfront_url" {
+  value = "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"
+}
+
+output "app_bucket" {
+  value = aws_s3_bucket.app.bucket
+}
