0c5d8010314c730500fbf384de50eda9486f7c46
David Adams <david.adams.16.12@gmail.com>
2020-05-25 23:06:01 +1000

    Lift provider too

--- infra/main.tf
+++ infra/main.tf
@@ -1,30 +1,3 @@
-provider "aws" {
-  region  = "ap-southeast-2"
-  version = "~> 2.52"
-
-  endpoints {
-    apigateway     = lookup(var.endpoints, "apigateway", null)
-    cloudformation = lookup(var.endpoints, "cloudformation", null)
-    cloudwatch     = lookup(var.endpoints, "cloudwatch", null)
-    dynamodb       = lookup(var.endpoints, "dynamodb", null)
-    es             = lookup(var.endpoints, "es", null)
-    firehose       = lookup(var.endpoints, "firehose", null)
-    iam            = lookup(var.endpoints, "iam", null)
-    kinesis        = lookup(var.endpoints, "kinesis", null)
-    lambda         = lookup(var.endpoints, "lambda", null)
-    route53        = lookup(var.endpoints, "route53", null)
-    redshift       = lookup(var.endpoints, "redshift", null)
-    s3             = lookup(var.endpoints, "s3", null)
-    secretsmanager = lookup(var.endpoints, "secretsmanager", null)
-    ses            = lookup(var.endpoints, "ses", null)
-    sns            = lookup(var.endpoints, "sns", null)
-    sqs            = lookup(var.endpoints, "sqs", null)
-    ssm            = lookup(var.endpoints, "ssm", null)
-    stepfunctions  = lookup(var.endpoints, "stepfunctions", null)
-    sts            = lookup(var.endpoints, "sts", null)
-  }
-}
-
 locals {
   environment = terraform.workspace == "default" ? "production" : terraform.workspace
 }
