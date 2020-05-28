55adcd463c1f727722f4588e427462b613400c40
David Adams <david.adams.16.12@gmail.com>
2020-05-29 08:57:14 +1000

    Shuffle resource order

--- infra/app/dns.tf
+++ infra/app/dns.tf
@@ -5,14 +5,6 @@ resource "cloudflare_record" "root_domain" {
   name    = var.subdomain
   value   = aws_cloudfront_distribution.s3_distribution.domain_name
 }
-
-resource "cloudflare_record" "root_cert_validation" {
-  zone_id = var.cloudflare_zone_id
-  name    = aws_acm_certificate.root_cert.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.root_cert.domain_validation_options.0.resource_record_type
-  value   = trimsuffix(aws_acm_certificate.root_cert.domain_validation_options.0.resource_record_value, ".")
-}
-
 resource "aws_acm_certificate" "root_cert" {
   domain_name       = var.domain
   validation_method = "DNS"
@@ -25,4 +17,11 @@ resource "aws_acm_certificate_validation" "root_cert" {
   validation_record_fqdns = [cloudflare_record.root_cert_validation.hostname]
 
   provider = aws.us_east_1
+}
+
+resource "cloudflare_record" "root_cert_validation" {
+  zone_id = var.cloudflare_zone_id
+  name    = aws_acm_certificate.root_cert.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.root_cert.domain_validation_options.0.resource_record_type
+  value   = trimsuffix(aws_acm_certificate.root_cert.domain_validation_options.0.resource_record_value, ".")
 }
