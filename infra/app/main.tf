12fe06ad2f456f7f0bfb3e49484075cdd106b259
David Adams <david.adams.16.12@gmail.com>
2020-05-28 19:40:06 +1000

    Use custom domain cert

--- infra/app/main.tf
+++ infra/app/main.tf
@@ -70,7 +70,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = aws_acm_certificate.root_cert.arn
+    ssl_support_method = "sni-only"
   }
 }
 
