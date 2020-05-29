e900a8d5016041812359ff74f1b1ed8c115ce00d
David Adams <david.adams.16.12@gmail.com>
2020-05-30 09:04:57 +1000

    Redirect www to primary domain in production

--- infra/app/dns.tf
+++ infra/app/dns.tf
@@ -1,10 +1,11 @@
 # Root domain DNS and SSL Cert
-resource "cloudflare_record" "root_domain" {
+resource "cloudflare_record" "primary_cname" {
   zone_id = var.cloudflare_zone_id
   type    = "CNAME"
   name    = var.subdomain
   value   = aws_cloudfront_distribution.s3_distribution.domain_name
 }
+
 resource "aws_acm_certificate" "root_cert" {
   domain_name       = var.domain
   validation_method = "DNS"
@@ -24,4 +25,29 @@ resource "cloudflare_record" "root_cert_validation" {
   name    = aws_acm_certificate.root_cert.domain_validation_options.0.resource_record_name
   type    = aws_acm_certificate.root_cert.domain_validation_options.0.resource_record_type
   value   = trimsuffix(aws_acm_certificate.root_cert.domain_validation_options.0.resource_record_value, ".")
+}
+
+# Redirect www domain to root domain for production
+resource "cloudflare_record" "www_cname" {
+  count = var.environment == "production" ? 1 : 0
+
+  zone_id = var.cloudflare_zone_id
+  type    = "CNAME"
+  name    = "www"
+  value   = aws_cloudfront_distribution.s3_distribution.domain_name
+}
+
+resource "cloudflare_page_rule" "redirect_to_root" {
+  count = var.environment == "production" ? 1 : 0
+
+  zone_id = var.cloudflare_zone_id
+  target = "www.${var.domain}/*"
+  priority = 1
+
+  actions {
+    forwarding_url {
+      url = var.domain
+      status_code = 301
+    }
+  }
 }
