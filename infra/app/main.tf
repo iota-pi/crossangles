4318b98143977d3a68e946da2d8cc260f4b0aa70
David Adams <david.adams.16.12@gmail.com>
2020-06-02 07:34:10 +1000

    Remove trailing slash in origin_path

--- infra/app/main.tf
+++ infra/app/main.tf
@@ -29,7 +29,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name = aws_s3_bucket.app.bucket_regional_domain_name
     origin_id   = local.origin_id
-    origin_path = "/${var.git_version}/"
+    origin_path = "/${var.git_version}"
 
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.app_oai.cloudfront_access_identity_path
