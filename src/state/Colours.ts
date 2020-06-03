1de04698206f046ed372bb7852d95edffbdd92e7
David Adams <david.adams.16.12@gmail.com>
2020-06-03 20:54:32 +1000

    Hard-code colour names

--- src/state/Colours.ts
+++ src/state/Colours.ts
@@ -1,26 +1,20 @@
-import { pink, deepPurple, indigo, blue, teal, lightGreen, amber, deepOrange, lime } from '@material-ui/core/colors';
-
-const LIGHT_VARIANT = 700;
-const DARK_VARIANT = 600;
-
 const COLOUR_MAP = {
-  pink,
-  deepPurple,
-  indigo,
-  blue,
-  teal,
-  lightGreen,
-  amber,
-  deepOrange,
+  pink:       { light: '#c2185b', dark: '#d81b60' },
+  deepPurple: { light: '#512da8', dark: '#5e35b1' },
+  indigo:     { light: '#303f9f', dark: '#3949ab' },
+  blue:       { light: '#1976d2', dark: '#1e88e5' },
+  teal:       { light: '#00796b', dark: '#00897b' },
+  lightGreen: { light: '#689f38', dark: '#7cb342' },
+  amber:      { light: '#ffa000', dark: '#ffb300' },
+  deepOrange: { light: '#e64a19', dark: '#f4511e' },
 };
-export const FALLBACK_COLOUR = lime;
+export const FALLBACK_COLOUR = { light: '#afb42b', dark: '#c0ca33' };
 
 export type Colour = keyof typeof COLOUR_MAP;
 
 export const getColour = (colourName: Colour, dark?: boolean) => {
   const colour = COLOUR_MAP[colourName] || FALLBACK_COLOUR;
-  const variant = dark ? DARK_VARIANT : LIGHT_VARIANT;
-  return colour[variant];
+  return dark ? colour.dark : colour.light;
 }
 
 export const COURSE_COLOURS = Object.keys(COLOUR_MAP) as Colour[];
@@ -28,4 +22,3 @@ export const COURSE_COLOURS = Object.keys(COLOUR_MAP) as Colour[];
 export interface ColourMap {
   [course: string]: Colour,
 }
-
