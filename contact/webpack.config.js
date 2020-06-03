087ff7ad9c738e1a5583227a5b884e74773bfef5
David Adams <david.adams.16.12@gmail.com>
2020-06-03 21:51:11 +1000

    Slim lambda dependencies

--- contact/webpack.config.js
+++ contact/webpack.config.js
@@ -2,7 +2,6 @@ module.exports = {
   entry: ['./build/contact/index.js'],
   target: 'node',
   mode: 'production',
-  externals: ['aws-sdk'],
   output: {
     path: `${process.cwd()}/build/bundled`,
     filename: 'lambda.js',
