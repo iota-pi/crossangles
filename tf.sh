f64ebfc8926767227002d61d508c58f357c9a29e
David Adams <david.adams.16.12@gmail.com>
2020-06-03 22:16:24 +1000

    Only add -input=false for relevant commands

--- tf.sh
+++ tf.sh
@@ -1,6 +1,14 @@
+#!/usr/bin/env bash
+set -ex
+
+extra_args=""
+if [[ $1 =~ init|refresh|apply ]]; then
+  extra_args+=" -input=false"
+fi
+
 TF_VAR_app_version="$(git log -1 --pretty=tformat:%H app)"
 TF_VAR_scraper_version="$(git log -1 --pretty=tformat:%H scraper)"
 TF_VAR_contact_version="$(git log -1 --pretty=tformat:%H contact)"
 TF_VAR_image_version="$(git log -1 --pretty=tformat:%H image)"
 cd infra
-terraform $@ -input=false
+terraform $@ $extra_args
