5f145adb23a0240c5abefef38b23ecafe417f9aa
David Adams <david.adams.16.12@gmail.com>
2020-06-03 22:25:35 +1000

    Export TF_VAR env variables

--- tf.sh
+++ tf.sh
@@ -6,9 +6,9 @@ if [[ $1 =~ init|refresh|apply ]]; then
   extra_args+=" -input=false"
 fi
 
-TF_VAR_app_version="$(git log -1 --pretty=tformat:%H app)"
-TF_VAR_scraper_version="$(git log -1 --pretty=tformat:%H scraper)"
-TF_VAR_contact_version="$(git log -1 --pretty=tformat:%H contact)"
-TF_VAR_image_version="$(git log -1 --pretty=tformat:%H image)"
+export TF_VAR_app_version="$(git log -1 --pretty=tformat:%H app)"
+export TF_VAR_scraper_version="$(git log -1 --pretty=tformat:%H scraper)"
+export TF_VAR_contact_version="$(git log -1 --pretty=tformat:%H contact)"
+export TF_VAR_image_version="$(git log -1 --pretty=tformat:%H image)"
 cd infra
 terraform $@ $extra_args
