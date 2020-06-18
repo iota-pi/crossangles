8899cc7dcc16e673e532674984fee84ff45c11c9
David Adams <david.adams.16.12@gmail.com>
2020-06-18 22:14:24 +1000

    Also ignore other files in version calculation

--- version.sh
+++ version.sh
@@ -4,4 +4,5 @@ paths=$@
 if [[ $paths =~ contact|image ]]; then
   paths+=" lambda-shared"
 fi
-git log -1 --pretty="tformat:%at-%H" $paths ":(exclude)*.spec.[tj]s" ":(exclude)*.test.[tj]s"
+excluded_paths=':(exclude)*.spec.[tj]s :(exclude)*.test.[tj]s :(exclude)*.snap :(exclude)*.br :(exclude)*.gitignore'
+git log -1 --pretty="tformat:%at-%H" $paths $excluded_paths
