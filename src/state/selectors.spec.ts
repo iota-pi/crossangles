93bf64c9aef2b33f09e62c8539dd40f78585e97d
David Adams <david.adams.16.12@gmail.com>
2020-06-19 18:43:01 +1000

    Fix getShowSignup

---
+++ src/state/selectors.spec.ts
@@ -0,0 +1,30 @@
+import { _getShowSignup } from "./selectors"
+import { getCourse, getAdditionalCourse } from "../test_util"
+import { getCourseId } from "./Course";
+import { AdditionalEvent, getEvents } from "./Events";
+
+describe('getShowSignup', () => {
+  it.each`
+    show ${true} ${false}
+  `('shows and hides signup link correctly (show = $show)', ({ show }) => {
+    const courses = [getCourse(), getAdditionalCourse()];
+    const additionalId = getCourseId(courses[1]);
+    const courseMap = {
+      [getCourseId(courses[0])]: courses[0],
+      [additionalId]: courses[1],
+    };
+    const events: AdditionalEvent[] = show ? [getEvents(courses[1])[0]] : [];
+    expect(_getShowSignup(courseMap, [additionalId], events)).toBe(show);
+  })
+
+  it('hides signup link if no chosen events exist', () => {
+    const courses = [getCourse(), getAdditionalCourse()];
+    const additionalId = getCourseId(courses[1]);
+    const courseMap = {
+      [getCourseId(courses[0])]: courses[0],
+      [additionalId]: courses[1],
+    };
+    const events: AdditionalEvent[] = [{id: 'adlkjgsd', name: 'Does not exist'}];
+    expect(_getShowSignup(courseMap, [additionalId], events)).toBe(false);
+  })
+})
