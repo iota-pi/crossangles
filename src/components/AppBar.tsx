e7e40c021b124d1a73b2a2fcbdc51139a4a2d04d
David Adams <david.adams.16.12@gmail.com>
2020-08-18 21:21:04 +1000

    Include current term in AppBar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import createStyles from '@material-ui/core/styles/createStyles';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import Hidden from '@material-ui/core/Hidden';
 import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import Tooltip from '@material-ui/core/Tooltip';
+import Typography from '@material-ui/core/Typography';
 import InvertColors from '@material-ui/icons/InvertColors';
 import InvertColorsOff from '@material-ui/icons/InvertColorsOff';
-import { makeStyles } from '@material-ui/core';
 import { AppOptions } from './AppOptions';
 import { AboutCrossAngles } from './AboutCrossAngles';
+import { RootState } from '../state';
 import { getOptions } from '../state/selectors';
 import { toggleOption } from '../actions';
 
-const useStyles = makeStyles(() => createStyles({
+const useStyles = makeStyles(theme => ({
   grow: {
     flexGrow: 1,
+  },
+  term: {
+    marginRight: theme.spacing(1),
+    fontWeight: 300,
+    fontSize: '1.1rem',
+  },
+  termNumber: {
+    fontWeight: 500,
   },
 }));
 
@@ -30,6 +40,7 @@ export function CrossAnglesAppBar({
   const classes = useStyles();
   const dispatch = useDispatch();
   const { darkMode } = useSelector(getOptions);
+  const { term, year } = useSelector((state: RootState) => state.meta);
   const handleClickDarkMode = React.useCallback(
     () => dispatch(toggleOption('darkMode')),
     [dispatch],
@@ -45,6 +56,22 @@ export function CrossAnglesAppBar({
           CrossAngles
         </Typography>
 
+        <Tooltip
+          title={
+            `Course data is for Term ${term}, ${year}. `
+            + 'CrossAngles will automatically update when the data for the next term becomes available.'
+          }
+        >
+          <Typography color="inherit" className={classes.term}>
+            <Hidden xsDown>{'Term '}</Hidden>
+            <span className={classes.termNumber}>
+              <Hidden smUp>T</Hidden>
+              {term}
+            </span>
+            <Hidden xsDown>, {year}</Hidden>
+          </Typography>
+        </Tooltip>
+
         <IconButton onClick={handleClickDarkMode} color="inherit">
           {darkMode ? (
             <InvertColorsOff />
@@ -52,7 +79,6 @@ export function CrossAnglesAppBar({
             <InvertColors />
           )}
         </IconButton>
-
         <AboutCrossAngles onShowContact={onShowContact} />
         <AppOptions />
       </Toolbar>
