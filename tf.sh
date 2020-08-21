400b9b62a077f5ea8210297be4477506a8ffeda9
David Adams <david.adams.16.12@gmail.com>
2020-08-21 22:49:18 +1000

    Make TF less verbose

--- tf.sh
+++ tf.sh
@@ -10,5 +10,6 @@ export TF_VAR_app_version="$(./version.sh app)"
 export TF_VAR_scraper_version="$(./version.sh scraper)"
 export TF_VAR_contact_version="$(./version.sh contact)"
 export TF_VAR_image_version="$(./version.sh image)"
+export TF_IN_AUTOMATION=$CI
 cd infra
 terraform $@ $extra_args
