73af86a36d02ac992add6e421e4db6f38d09c45b
David Adams <david.adams.16.12@gmail.com>
2020-08-22 18:01:36 +1000

    Provide data configuration for USYD campus

--- app/src/campusConfig.ts
+++ app/src/campusConfig.ts
@@ -16,6 +16,11 @@ export const campusConfig: AllCampusConfig = {
     name: 'UNSW',
     longname: 'University of New South Wales',
   },
+  usyd: {
+    dataPath: `${DATA_ROOT_URI}/usyd/data.json`,
+    name: 'USYD',
+    longname: 'University of Sydney',
+  },
 };
 
 export default campusConfig;
