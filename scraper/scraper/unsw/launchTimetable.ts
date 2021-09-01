de9b2a3c7858195dfdef56f9e19ea7895ebcecee
David Adams <david.adams@cross-code.org>
2021-09-01 21:58:10 +1000

    Update local timetableScraper launch script

--- scraper/scraper/unsw/launchTimetable.ts
+++ scraper/scraper/unsw/launchTimetable.ts
@@ -4,9 +4,9 @@ import { TimetableScraper } from './TimetableScraper';
 const runScraper = async () => {
   const timetable = new TimetableScraper();
   timetable.state = undefined;
-  timetable.facultyLinkFilter = links => links.filter(l => l.includes('DESN'));
+  timetable.facultyLinkFilter = links => links.filter(l => l.includes('MATH'));
   await timetable.setup();
   const result = await timetable.scrape();
-  writeFileSync('test_timetable.json', JSON.stringify(result[2]));
+  writeFileSync('test_timetable.json', JSON.stringify(result, undefined, 2));
 };
 runScraper();
