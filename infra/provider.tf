00105f215c12c92b1afc239df4e6de61639aa83e
David Adams <david.adams.16.12@gmail.com>
2023-12-11 13:57:49 +1100

    Update terraform providers

--- infra/provider.tf
+++ infra/provider.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.28"
+      version = "~> 5.30"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 3.22"
+      version = "~> 4.20"
     }
   }
 
