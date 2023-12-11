15cce31927c7b6c972b50b98de79f655144338f0
David Adams <david.adams.16.12@gmail.com>
2023-12-11 19:01:23 +1100

    Switch to vite and update packages

--- deploy/deploy-app.sh
+++ deploy/deploy-app.sh
@@ -40,13 +40,13 @@ fi
 environment_hyphens=$(echo $environment | sed 's/./-/g')
 echo "Deploying app to $environment"
 echo "-----------------$environment_hyphens"
-export REACT_APP_BASE_URL=crossangles.app
+export VITE_BASE_URL=crossangles.app
 if [[ $environment != "production" ]]; then
-  export REACT_APP_BASE_URL=$environment.$REACT_APP_BASE_URL
+  export VITE_BASE_URL=$environment.$VITE_BASE_URL
 fi
-export REACT_APP_STAGE_NAME=$environment
-export REACT_APP_CONTACT_ENDPOINT=$contact_endpoint
-export REACT_APP_DATA_ROOT_URI=$data_uri
+export VITE_STAGE_NAME=$environment
+export VITE_CONTACT_ENDPOINT=$contact_endpoint
+export VITE_DATA_ROOT_URI=$data_uri
 
 max_age=0
 if [[ $environment == production ]]; then
@@ -57,11 +57,11 @@ cd ../app
 for campus in $@
 do
   if [[ $campus != "unsw" ]]; then
-    REACT_APP_BASE_URL=$campus.$REACT_APP_BASE_URL
+    VITE_BASE_URL=$campus.$VITE_BASE_URL
   fi
 
   echo "Building app for $campus"
-  REACT_APP_CAMPUS=$campus yarn build
+  VITE_CAMPUS=$campus yarn build
 
   campus_key_base="s3://$app_bucket/$campus"
   s3_app_params="--acl public-read --cache-control max-age=$max_age"
