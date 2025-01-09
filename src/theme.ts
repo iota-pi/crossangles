c5fd0842912b6730bff03f8f8e1c830a3d6c6f18
David Adams <david.adams@cross-code.org>
2025-01-09 18:29:55 +1100

    Remove unused imports

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,7 @@
 /* eslint-disable no-lonely-if */
 import { createTheme } from '@material-ui/core/styles';
 import indigo from '@material-ui/core/colors/indigo';
-import green from '@material-ui/core/colors/green';
 import lightBlue from '@material-ui/core/colors/blue';
-import lightGreen from '@material-ui/core/colors/lightGreen';
 import { PaletteOptions } from '@material-ui/core/styles/createPalette';
 
 export const theme = (dark = false) => {
