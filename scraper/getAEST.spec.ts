668c48799a0ec04904e720b02588df5f892edd6f
David Adams <david.adams@cross-code.org>
2025-02-04 15:53:04 +1100

    Fix a slightly flaky test

--- scraper/getAEST.spec.ts
+++ scraper/getAEST.spec.ts
@@ -4,5 +4,5 @@ it('returns expected time', () => {
   // Test must be run in AEST timezone
   const expected = new Date().getTime();
   const result = getAEST().toDate().getTime();
-  expect(result - expected).toBeLessThanOrEqual(5);
+  expect(result - expected).toBeLessThanOrEqual(100);
 });
