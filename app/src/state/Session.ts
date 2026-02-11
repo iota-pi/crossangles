90975788c880d6b8a1604024f1e29501b8120a84
arcu-pc <arcueun@gmail.com>
2026-02-12 00:55:04 +1100

    refactor: removed repetition of ALL_DAYS

--- app/src/state/Session.ts
+++ app/src/state/Session.ts
@@ -5,7 +5,7 @@ export type SessionId = string
 
 export type DayLetter = 'M' | 'T' | 'W' | 'H' | 'F' | 'S' | 's'
 export const WEEKDAY_DAYS: DayLetter[] = ['M', 'T', 'W', 'H', 'F']
-export const ALL_DAYS: DayLetter[] = ['M', 'T', 'W', 'H', 'F', 'S', 's']
+export const ALL_DAYS: DayLetter[] = [...WEEKDAY_DAYS, 'S', 's'];
 
 export interface SessionCommon {
   index: number,
